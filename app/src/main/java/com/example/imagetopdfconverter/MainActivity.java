f7e33f1ceb3633eb14cf7374be6025d8552f4ba2
dazz2014 <dasuneranda@gmail.com>
2020-07-01 20:13:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imagetopdfconverter/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.imagetopdfconverter;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int GALLERY_INTENT = 120;
+    ImageView imageView;
+    Button galleryBtn,convertBtn;
+    Bitmap bitmap;
+    private File pdfPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.image_view);
+        galleryBtn = findViewById(R.id.galleryBtn);
+        convertBtn = findViewById(R.id.convertBtn);
+
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                String imageFileName = "PDF_" + timeStamp + "_";
+                File storageDir = getAlbumDir();
+                try {
+
+                  pdfPath = File.createTempFile(
+                            imageFileName, /* prefix */
+                            ".pdf", /* suffix */
+                            storageDir  /* directory */
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(photoPickerIntent, GALLERY_INTENT);
+            }
+        });
+
+
+        convertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bitmap == null) {
+                    Toast.makeText(MainActivity.this, "Please select the image from gallery", Toast.LENGTH_LONG).show();
+                } else {
+
+
+                    convertToPDF(pdfPath);
+                }
+            }
+        });
+
+    }
+
+
+    private File getAlbumDir() {
+        File storageDir = null;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            storageDir = new File(Environment.getExternalStorageDirectory()
+                    + "/dcim/"
+                    + "Image to pdf");
+            if (!storageDir.mkdirs()) {
+                if (!storageDir.exists()) {
+                    Log.d("CameraSample", "failed to create directory");
+                    return null;
+                }
+            }
+        } else {
+            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+        }
+        return storageDir;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_INTENT) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                if (selectedImage != null) {
+                    Cursor cursor = getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+                    if (cursor != null) {
+                        cursor.moveToFirst();
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        String imagePath = cursor.getString(columnIndex);
+                        bitmap = BitmapFactory.decodeFile(imagePath);
+                        imageView.setImageBitmap(bitmap);
+                        cursor.close();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.e("Canceled", "Image not selected");
+            }
+        }
+    }
+
+    private void convertToPDF(File pdfPath) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        PdfDocument pdfDocument = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(width, height, 1).create();
+        PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#ffffff"));
+        canvas.drawPaint(paint);
+
+        bitmap = Bitmap.createScaledBitmap(bitmap, width, height, true);
+        paint.setColor(Color.BLUE);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        pdfDocument.finishPage(page);
+
+        try {
+            pdfDocument.writeTo(new FileOutputStream(pdfPath));
+            Toast.makeText(MainActivity.this, "Image is successfully converted to PDF", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pdfDocument.close();
+    }
+
+
+}
